8c53ba5026b9e5d9bfde1d2ef0d5d6c3d7d14ca1
Duncan Joyce <Duncan_Joyce@waters.com>
2020-01-21 10:47:43 +0000

    Activated story 5 tests, created toggle for story 5

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -6,10 +6,19 @@ namespace TaxCalculator.Tests
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
+        private bool story5;
+
+        public DefaultTaxCalculator(bool story5 = false)
+        {
+            this.story5 = story5;
+        }
+
         public override int CalculateTax(Vehicle vehicle)
         {
             var emissions = vehicle.Co2Emissions;
             var fuelType = vehicle.FuelType;
+            var listPrice = vehicle.ListPrice;
+            var year = vehicle.DateOfFirstRegistration.Year;
             var cost = 0;
             Dictionary<int, int> index = null;
 
