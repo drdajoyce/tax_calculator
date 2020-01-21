411a7083505455919d2affbd7103fb07f24ab75f
Duncan Joyce <Duncan_Joyce@waters.com>
2020-01-21 12:29:54 +0000

    Reverter back to story 5 implementation

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -6,66 +6,84 @@ namespace TaxCalculator.Tests
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
-        private bool story4;
+        private bool story5;
 
-        public DefaultTaxCalculator (bool story4 = false)
-	{
-        this.story4 = story4;
-	}
+        public DefaultTaxCalculator(bool story5 = false)
+        {
+            this.story5 = story5;
+        }
 
         public override int CalculateTax(Vehicle vehicle)
         {
 
             var emissions = vehicle.Co2Emissions;
             var fuelType = vehicle.FuelType;
+            var year = vehicle.DateOfFirstRegistration.Year;
+            var listPrice = vehicle.ListPrice;
             var cost = 0;
             Dictionary<int, int> index = null;
 
-            if (vehicle.DateOfFirstRegistration )
+            if (story5)
             {
-            }
-
-
-
-            if (fuelType.Equals(FuelType.Electric))
-            {
-                return 0;
-            }
-            else if(fuelType.Equals(FuelType.Petrol))
-            {
-                foreach (var taxband in PetrolPriceIndex.index)
+                if (!year.Equals(2019) && listPrice > 40000)
                 {
-                    if(emissions <= taxband.Key)
+                    if (fuelType.Equals(FuelType.Petrol) || fuelType.Equals(FuelType.Diesel))
                     {
-                        return taxband.Value;
+                        return 450;
+                    }
+                    else if (fuelType.Equals(FuelType.Electric))
+                    {
+                        return 310;
+                    }
+                    else if (fuelType.Equals(FuelType.AlternativeFuel))
+                    {
+                        return 440;
                     }
                 }
-                return cost;
             }
-            else if(fuelType.Equals(FuelType.Diesel))
+            else
             {
-                index = DieselPriceIndex.index;
-                foreach (var taxband in index)
+
+                if (fuelType.Equals(FuelType.Electric))
                 {
-                    if(emissions <= taxband.Key)
-                    {
-                        return taxband.Value;
-                    }
+                    return 0;
                 }
+                else if (fuelType.Equals(FuelType.Petrol))
+                {
+                    foreach (var taxband in PetrolPriceIndex.index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
+                else if (fuelType.Equals(FuelType.Diesel))
+                {
+                    index = DieselPriceIndex.index;
+                    foreach (var taxband in index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
+                else if (fuelType.Equals(FuelType.AlternativeFuel))
+                {
+                    foreach (var taxband in AlternativeFuelPriceIndex.index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
+            }
                 return cost;
             }
-            else if (fuelType.Equals(FuelType.AlternativeFuel))
-            {
-                foreach (var taxband in AlternativeFuelPriceIndex.index)
-                {
-                    if (emissions <= taxband.Key)
-                    {
-                        return taxband.Value;
-                    }
-                }
-                return cost;
-            }
-            return cost;
         }
     }
-}
