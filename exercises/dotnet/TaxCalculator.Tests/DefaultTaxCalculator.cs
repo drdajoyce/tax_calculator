d38b96b49451ca3689d3fc57ceff489dfb108d68
louistracey <57895991+louistracey@users.noreply.github.com>
2020-01-21 12:31:28 +0000

    made constructor

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -6,64 +6,102 @@ namespace TaxCalculator.Tests
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
+        private bool story5;
         private bool story4;
 
-        public DefaultTaxCalculator (bool story4 = false)
-	{
-        this.story4 = story4;
-	}
 
+        public DefaultTaxCalculator(bool story4 = false, bool story5 = false)
+        {
+            this.story4 = story4;
+            this.story5 = story5;
+        }
         public override int CalculateTax(Vehicle vehicle)
         {
 
             var emissions = vehicle.Co2Emissions;
             var fuelType = vehicle.FuelType;
+            var listPrice = vehicle.ListPrice;
+            var year = vehicle.DateOfFirstRegistration.Year;
             var cost = 0;
             Dictionary<int, int> index = null;
-
-            if (vehicle.DateOfFirstRegistration )
+            if (story5)
             {
-            }
-
-
-
-            if (fuelType.Equals(FuelType.Electric))
-            {
-                return 0;
-            }
-            else if(fuelType.Equals(FuelType.Petrol))
-            {
-                foreach (var taxband in PetrolPriceIndex.index)
+                if (!year.Equals(2019) && listPrice > 40000)
                 {
-                    if(emissions <= taxband.Key)
+                    if (fuelType.Equals(FuelType.Petrol) || fuelType.Equals(FuelType.Diesel))
                     {
-                        return taxband.Value;
+                        return 450;
+                    }
+                    else if (fuelType.Equals(FuelType.Electric))
+                    {
+                        return 310;
+                    }
+                    else if (fuelType.Equals(FuelType.AlternativeFuel))
+                    {
+                        return 440;
                     }
                 }
-                return cost;
             }
-            else if(fuelType.Equals(FuelType.Diesel))
+            else if (story4)
             {
-                index = DieselPriceIndex.index;
-                foreach (var taxband in index)
+                if (!year.Equals(2019))
                 {
-                    if(emissions <= taxband.Key)
+                    if (fuelType.Equals(FuelType.Petrol) || fuelType.Equals(FuelType.Diesel))
                     {
-                        return taxband.Value;
+                        return 140;
+                    }
+                    else if (fuelType.Equals(FuelType.Electric))
+                    {
+                        return 0;
+                    }
+                    else if (fuelType.Equals(FuelType.AlternativeFuel))
+                    {
+                        return 130;
                     }
                 }
-                return cost;
+
             }
-            else if (fuelType.Equals(FuelType.AlternativeFuel))
+
+            else
             {
-                foreach (var taxband in AlternativeFuelPriceIndex.index)
+                if (fuelType.Equals(FuelType.Electric))
                 {
-                    if (emissions <= taxband.Key)
-                    {
-                        return taxband.Value;
-                    }
+                    return 0;
                 }
-                return cost;
+                else if (fuelType.Equals(FuelType.Petrol))
+                {
+                    foreach (var taxband in PetrolPriceIndex.index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
+                else if (fuelType.Equals(FuelType.Diesel))
+                {
+                    index = DieselPriceIndex.index;
+                    foreach (var taxband in index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
+                else if (fuelType.Equals(FuelType.AlternativeFuel))
+                {
+                    foreach (var taxband in AlternativeFuelPriceIndex.index)
+                    {
+                        if (emissions <= taxband.Key)
+                        {
+                            return taxband.Value;
+                        }
+                    }
+                    return cost;
+                }
             }
             return cost;
         }
