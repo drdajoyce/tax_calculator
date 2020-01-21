81e14c3850e97fbc0d0221f9f7884d65f3027c31
louistracey <57895991+louistracey@users.noreply.github.com>
2020-01-21 12:32:26 +0000

    new construct

--- exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator.Tests/DefaultTaxCalculator.cs
@@ -6,102 +6,61 @@ namespace TaxCalculator.Tests
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
-        private bool story5;
         private bool story4;
+        private bool story5;
 
+        public DefaultTaxCalculator (bool story4 = false, bool story5 = false)
+	{
+        this.story4 = story4;
+        this.story5 = story5;
 
-        public DefaultTaxCalculator(bool story4 = false, bool story5 = false)
-        {
-            this.story4 = story4;
-            this.story5 = story5;
         }
+
         public override int CalculateTax(Vehicle vehicle)
         {
 
             var emissions = vehicle.Co2Emissions;
             var fuelType = vehicle.FuelType;
-            var listPrice = vehicle.ListPrice;
-            var year = vehicle.DateOfFirstRegistration.Year;
             var cost = 0;
             Dictionary<int, int> index = null;
-            if (story5)
-            {
-                if (!year.Equals(2019) && listPrice > 40000)
-                {
-                    if (fuelType.Equals(FuelType.Petrol) || fuelType.Equals(FuelType.Diesel))
-                    {
-                        return 450;
-                    }
-                    else if (fuelType.Equals(FuelType.Electric))
-                    {
-                        return 310;
-                    }
-                    else if (fuelType.Equals(FuelType.AlternativeFuel))
-                    {
-                        return 440;
-                    }
-                }
-            }
-            else if (story4)
-            {
-                if (!year.Equals(2019))
-                {
-                    if (fuelType.Equals(FuelType.Petrol) || fuelType.Equals(FuelType.Diesel))
-                    {
-                        return 140;
-                    }
-                    else if (fuelType.Equals(FuelType.Electric))
-                    {
-                        return 0;
-                    }
-                    else if (fuelType.Equals(FuelType.AlternativeFuel))
-                    {
-                        return 130;
-                    }
-                }
 
-            }
-
-            else
+            if (fuelType.Equals(FuelType.Electric))
             {
-                if (fuelType.Equals(FuelType.Electric))
+                return 0;
+            }
+            else if(fuelType.Equals(FuelType.Petrol))
+            {
+                foreach (var taxband in PetrolPriceIndex.index)
                 {
-                    return 0;
-                }
-                else if (fuelType.Equals(FuelType.Petrol))
-                {
-                    foreach (var taxband in PetrolPriceIndex.index)
+                    if(emissions <= taxband.Key)
                     {
-                        if (emissions <= taxband.Key)
-                        {
-                            return taxband.Value;
-                        }
+                        return taxband.Value;
                     }
-                    return cost;
                 }
-                else if (fuelType.Equals(FuelType.Diesel))
+                return cost;
+            }
+            else if(fuelType.Equals(FuelType.Diesel))
+            {
+                index = DieselPriceIndex.index;
+                foreach (var taxband in index)
                 {
-                    index = DieselPriceIndex.index;
-                    foreach (var taxband in index)
+                    if(emissions <= taxband.Key)
                     {
-                        if (emissions <= taxband.Key)
-                        {
-                            return taxband.Value;
-                        }
+                        return taxband.Value;
                     }
-                    return cost;
                 }
-                else if (fuelType.Equals(FuelType.AlternativeFuel))
+                return cost;
+            }
+            else if (fuelType.Equals(FuelType.AlternativeFuel))
+            {
+                foreach (var taxband in AlternativeFuelPriceIndex.index)
                 {
-                    foreach (var taxband in AlternativeFuelPriceIndex.index)
+                    if (emissions <= taxband.Key)
                     {
-                        if (emissions <= taxband.Key)
-                        {
-                            return taxband.Value;
-                        }
+                        return taxband.Value;
                     }
-                    return cost;
                 }
+                return cost;
             }
             return cost;
         }
